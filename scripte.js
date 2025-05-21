d22eafcb2bbd4048243fbcf5bd544922afcb3dc1
hicam-art <hichamtahar797@gmail.com>
2025-05-21 15:06:34 -0700

    Add files via upload

--- scripte.js
+++ scripte.js
@@ -1,5 +1,26 @@
 function a(img){
 let amm=document.querySelector('#back');
-amm.setAttribute('src',img)
-
+amm.setAttribute('src',img);
 }
+
+function a1(img){
+let amm=document.querySelector('#back1');
+amm.setAttribute('src',img);
+}
+
+function a2(img){
+let amm=document.querySelector('#back2');
+amm.setAttribute('src',img);
+}
+
+
+function a3(img){
+let amm=document.querySelector('#back3');
+amm.setAttribute('src',img);
+}
+
+function a4(img){
+let amm=document.querySelector('#back4');
+amm.setAttribute('src',img);
+}
+
